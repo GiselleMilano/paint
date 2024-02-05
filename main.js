0f7a8533b05c3993da414e05a58ea1be5d84ff94
Giselle <milanoregina8@gmail.com>
2024-02-05 14:56:34 -0300

    add events to drawing and stop drawing

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-function draw() {
-  const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let isDrawing = false;
+let brushSize = 4;
 
-  if (canvas.getContext) {
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  }
+function drawCanvas() {
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-window.addEventListener('load', draw);
+function drawing(event) {
+  if (!isDrawing) return;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+function startDrawing() {
+  isDrawing = true;
+  ctx.beginPath();
+  ctx.lineWidth = brushSize;
+}
+function stopDrawing() {
+  isDrawing = false;
+}
+
+window.addEventListener('load', drawCanvas);
+window.addEventListener('mousedown', startDrawing);
+window.addEventListener('mousemove', drawing);
+window.addEventListener('mouseup', stopDrawing);
