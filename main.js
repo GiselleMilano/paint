e7693e5c790abc05b6ea464ca6e98b37afa394a1
Giselle <milanoregina8@gmail.com>
2024-02-05 16:19:19 -0300

    I added events to detect if the mouse is inside or not before drawing

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 let isDrawing = false;
 let brushSize = 4;
+let mouseInside = false;
 
 function drawCanvas() {
   canvas.width = canvas.offsetWidth;
@@ -13,6 +14,7 @@ function drawCanvas() {
 
 function drawing(event) {
   if (!isDrawing) return;
+  if (!mouseInside) return;
   ctx.lineTo(event.offsetX, event.offsetY);
   ctx.stroke();
 }
@@ -31,6 +33,14 @@ function onSizeBrushChange(value) {
   brushSize = Number(value);
   document.getElementById('brush-size-info').textContent = value;
 }
+
+canvas.onmousedown = function () {
+  mouseInside = true;
+};
+
+canvas.onmouseup = function () {
+  mouseInside = false;
+};
 
 window.addEventListener('load', drawCanvas);
 window.addEventListener('mousedown', startDrawing);
