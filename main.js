bd323a9031e4194050a4ad5aa83f4e1a0259bdb1
Giselle <milanoregina8@gmail.com>
2024-02-06 15:56:36 -0300

    New styles to options and options switch from js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
 let isDrawing = false;
 let brushSize = 4;
 let mouseInside = false;
+let optionSelected = 1;
 
 function drawCanvas() {
   canvas.width = canvas.offsetWidth;
@@ -15,7 +17,14 @@ function drawCanvas() {
 function drawing(event) {
   if (!isDrawing) return;
   if (!mouseInside) return;
+
   ctx.lineTo(event.offsetX, event.offsetY);
+
+  if (optionSelected === 2) {
+    ctx.strokeStyle = '#fff';
+  } else {
+    ctx.strokeStyle = '#000000';
+  }
   ctx.stroke();
 }
 
@@ -23,6 +32,12 @@ function startDrawing() {
   isDrawing = true;
   ctx.beginPath();
   ctx.lineWidth = brushSize;
+
+  if (optionSelected === 2) {
+    ctx.strokeStyle = '#fff';
+  } else {
+    ctx.strokeStyle = '#000000';
+  }
 }
 
 function stopDrawing() {
@@ -41,6 +56,25 @@ canvas.onmousedown = function () {
 canvas.onmouseup = function () {
   mouseInside = false;
 };
+
+function optionChange(value) {
+  optionSelected = Number(value);
+
+  let pencilContainer = document.getElementById('pencil-container');
+  let eraserContainer = document.getElementById('eraser-container');
+
+  if (optionSelected === 1) {
+    pencilContainer.classList.add('option-selected');
+  } else {
+    pencilContainer.classList.remove('option-selected');
+  }
+
+  if (optionSelected === 2) {
+    eraserContainer.classList.add('option-selected');
+  } else {
+    eraserContainer.classList.remove('option-selected');
+  }
+}
 
 window.addEventListener('load', drawCanvas);
 window.addEventListener('mousedown', startDrawing);
