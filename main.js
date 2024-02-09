a69530d336a8fd2be198011da1c011887cfaf412
Giselle <milanoregina8@gmail.com>
2024-02-09 18:09:09 -0300

    New download button to save the canvas. - I changed the name of the functions. - I changed window to document to events.

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let mouseInside = false;
 let optionSelected = 1;
 let colorBrush = '#000000';
 
+// Set canvas size and fill canvas rect
 function drawCanvas() {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
@@ -15,6 +16,7 @@ function drawCanvas() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+// Draw a path when stop is not and the mouse is down
 function drawing(event) {
   if (!isDrawing) return;
   if (!mouseInside) return;
@@ -29,6 +31,7 @@ function drawing(event) {
   ctx.stroke();
 }
 
+// Draw a new path when the mouse is already down
 function startDrawing() {
   isDrawing = true;
   ctx.beginPath();
@@ -41,13 +44,15 @@ function startDrawing() {
   }
 }
 
+// Stop function drawing and startDrawing
 function stopDrawing() {
   isDrawing = false;
 }
 
-function onSizeBrushChange(value) {
+// change the size of the brush (eraser or pencil)
+function changeSizeBrush(value) {
   brushSize = Number(value);
-  document.getElementById('brush-size-info').textContent = value;
+  document.getElementById('brush-size-info').textContent = brushSize;
 }
 
 canvas.onmousedown = function () {
@@ -58,18 +63,19 @@ canvas.onmouseup = function () {
   mouseInside = false;
 };
 
+// Delete canvas rect and fill a new canvas rect
 function removeCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = 'white';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function optionChange(value) {
+// Change pencil to eraser or vice versa
+function changeTool(value) {
   optionSelected = Number(value);
 
   let pencilContainer = document.getElementById('pencil-container');
   let eraserContainer = document.getElementById('eraser-container');
-  let clearContainer = document.getElementById('clear-container');
 
   if (optionSelected === 1) {
     pencilContainer.classList.add('option-selected');
@@ -82,18 +88,31 @@ function optionChange(value) {
   } else {
     eraserContainer.classList.remove('option-selected');
   }
-
-  if (optionSelected === 3) {
-    removeCanvas();
-  }
 }
 
-function onColorBrushChange(value) {
+// Update color piked to input element
+function changeColorBrush(value) {
   colorBrush = value;
   document.getElementById('colorBrush').textContent = value;
 }
 
+// Save | Download image
+function downloadImage(data, filename = 'untitled.png') {
+  var a = document.createElement('a');
+  a.href = data;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+}
+
+// Convert canvas to image
+function saveCanvas() {
+  const dataURL = canvas.toDataURL('image/png', 1.0);
+  downloadImage(dataURL, 'my-canvas.png');
+}
+
+// EVENTS
 window.addEventListener('load', drawCanvas);
-window.addEventListener('mousedown', startDrawing);
-window.addEventListener('mousemove', drawing);
-window.addEventListener('mouseup', stopDrawing);
+document.addEventListener('mousedown', startDrawing);
+document.addEventListener('mousemove', drawing);
+document.addEventListener('mouseup', stopDrawing);
